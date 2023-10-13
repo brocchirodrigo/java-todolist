6c1b38556af693c5cd0e2073c7a1a4b74d7af9e8
brocchirodrigo <rodrigo.brocchi@gmail.com>
2023-10-13 09:14:28 -0300

    Autenticação de rota com task setando idUser

--- src/main/java/br/com/imaginer/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/imaginer/todolist/filter/FilterTaskAuth.java
@@ -24,36 +24,58 @@ public class FilterTaskAuth extends OncePerRequestFilter {
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
 
-    var authorization = request.getHeader("Authorization");
+    var servletPath = request.getServletPath();
 
-    var token = authorization.substring("Basic".length()).trim();
+    if (servletPath.equals("/tasks/")) {
 
-    byte[] authDecode = Base64.getDecoder().decode(token);
+      var authorization = request.getHeader("Authorization");
 
-    var authString = new String(authDecode);
-
-    String[] credentials = authString.split(":");
-
-    String username = credentials[0];
-
-    String password = credentials[1];
-
-    var user = this.userRepository.findByUsername(username);
-
-    if (user == null) {
-      response.sendError(401, "Not authorized.");
-    } else {
-      var verifyPassword = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-
-      if (verifyPassword.verified) {
-
-        filterChain.doFilter(request, response);
-
-      } else {
+      if (authorization == null) {
 
         response.sendError(401, "Not authorized.");
 
+      } else {
+
+        var token = authorization.substring("Basic".length()).trim();
+
+        byte[] authDecode = Base64.getDecoder().decode(token);
+
+        var authString = new String(authDecode);
+
+        String[] credentials = authString.split(":");
+
+        String username = credentials[0];
+
+        String password = credentials[1];
+
+        var user = this.userRepository.findByUsername(username);
+
+        if (user == null) {
+
+          response.sendError(401, "Not authorized.");
+
+        } else {
+
+          var verifyPassword = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+          if (verifyPassword.verified) {
+
+            request.setAttribute("idUser", user.getId());
+
+            filterChain.doFilter(request, response);
+
+          } else {
+
+            response.sendError(401, "Not authorized.");
+
+          }
+
+        }
       }
+
+    } else {
+
+      filterChain.doFilter(request, response);
 
     }
 
